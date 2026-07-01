815eb4cd0954b19a2fbd088f49cb34ccdc9c9e86
KiruthikaSrinivasan <132181034+KiruthikaSrinivasan@users.noreply.github.com>
2026-07-01 17:00:05 +0300

    Update server.js

--- nodejswithdocker/server.js
+++ nodejswithdocker/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json(  'Hello World' );
+  res.send(  'Hello World' );
 });
 
 const port = process.env.PORT || 3000;
