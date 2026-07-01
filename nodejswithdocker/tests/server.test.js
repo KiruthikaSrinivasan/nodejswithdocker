7dc25d30ad7b325171aa8d1c222b05b71d1e4dec
Kiruthika <kiruthikas98@gmail.com>
2026-07-01 17:26:39 +0300

    resolved again

--- nodejswithdocker/tests/server.test.js
+++ nodejswithdocker/tests/server.test.js
@@ -1,14 +1,5 @@
 const request = require('supertest');
 const app = require('../server');
-let server;
-
-beforeAll(() => {
-  server = app.listen(3000);
-});
-
-afterAll(() => {
-  server.close();
-});
 describe('GET /', () => {
   it('should return Hello World message', async () => {
     const res = await request(app).get('/');
