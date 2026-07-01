219a47f12c6987a8706cbd00e2b1a0308404633e
Kiruthika <kiruthikas98@gmail.com>
2026-07-01 17:48:45 +0300

    content changed

--- nodejswithdocker/tests/server.test.js
+++ nodejswithdocker/tests/server.test.js
@@ -4,6 +4,6 @@ describe('GET /', () => {
   it('should return Hello World message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World');
+    expect(res.text).toBe('Hello');
   });
 });
